f18750339af5c7b4faae237322e43fa1d54dd6ac
Samuel Othieno <douglasothieno@gmail.com>
2024-01-30 21:24:03 +0300

    Inintial Commit:Add Develop Branch and React installation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+  return (
+    <MantineProvider>
+      
+    </MantineProvider>
+
+  )
+}
+
+export default App
