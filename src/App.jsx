dfe507d96733da9bc4c154818a12c7377b96008e
Samuel Othieno <douglasothieno@gmail.com>
2024-02-05 16:03:27 +0300

    ADd UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { NavbarSearch } from './Components/Home/Home';
 
 function App() {
   return (
     <MantineProvider>
-      
+      <NavbarSearch/>
     </MantineProvider>
 
   )
